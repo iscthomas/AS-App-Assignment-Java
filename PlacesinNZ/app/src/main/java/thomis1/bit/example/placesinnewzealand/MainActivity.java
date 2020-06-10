5de0ca31c9976ddafb9ea54ce8dda44780e8d33c
Isaac Thomas <50122666+iscthomas@users.noreply.github.com>
2020-06-11 11:26:34 +1200

    adjusted layout for 4 pane question quiz

--- PlacesinNZ/app/src/main/java/thomis1/bit/example/placesinnewzealand/MainActivity.java
+++ PlacesinNZ/app/src/main/java/thomis1/bit/example/placesinnewzealand/MainActivity.java
@@ -2,6 +2,7 @@ package thomis1.bit.example.placesinnewzealand;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int questionNumber = 0;
     Question[] questions;
     int score = 0;
+    TextView iv1;
+    TextView iv2;
+    TextView iv3;
+    TextView iv4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnNext.setOnClickListener(this);
         setUpQuestions();
         showQuestion();
+        setContentView(R.layout.activity_main);
+        iv1 = (TextView) findViewById(R.id.textView1);
+        iv1.setOnClickListener(this);
+        iv2 = (TextView) findViewById(R.id.textView2);
+        iv2.setOnClickListener(this);
+        iv3 = (TextView) findViewById(R.id.textView3);
+        iv3.setOnClickListener(this);
+        iv4 = (TextView) findViewById(R.id.textView4);
+        iv4.setOnClickListener(this);
     }
 
 
@@ -69,6 +83,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent changeActivity = new Intent(MainActivity.this, Results.class);
             changeActivity.putExtra("results", score);
             startActivity(changeActivity);
+        }
+        ////
+        if (v.getId()==R.id.textView1) {
+            iv1.setGravity(Gravity.CENTER);
+            iv1.setBackground(null);
+            iv1.setText("Wrong\nbeef\nCarne de res");
+        }
+        else   if (v.getId()==R.id.textView2)
+        {
+            iv2.setGravity(Gravity.CENTER);
+            iv2.setBackground(null);
+            iv2.setText("Wrong\nchicken\nRoasted");
+        }
+        else   if (v.getId()==R.id.textView3)
+        {
+            iv3.setGravity(Gravity.CENTER);
+            iv3.setBackground(null);
+            iv3.setText("Wrong\nlamb");
+        }
+        else   if (v.getId()==R.id.textView4)
+        {
+            iv4.setGravity(Gravity.CENTER);
+            iv4.setBackground(null);
+            iv4.setText("Correct\nGuinea Pig");
         }
     }
 }
