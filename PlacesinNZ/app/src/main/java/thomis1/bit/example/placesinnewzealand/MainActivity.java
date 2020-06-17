3a59ed36457bfa1d5fa19343dae4cda476ced8ba
Isaac Thomas <50122666+iscthomas@users.noreply.github.com>
2020-06-17 19:07:29 +1200

    add feedback for wrong answers

--- PlacesinNZ/app/src/main/java/thomis1/bit/example/placesinnewzealand/MainActivity.java
+++ PlacesinNZ/app/src/main/java/thomis1/bit/example/placesinnewzealand/MainActivity.java
@@ -27,10 +27,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView iv2;
     TextView iv3;
     TextView iv4;
-    TextView ivA;
     TextView question;
     Boolean correct = true;
-    Button buttonNext;
+    TextView textWrong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +44,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         iv3.setOnClickListener(this);
         iv4 = (TextView) findViewById(R.id.textView4);
         iv4.setOnClickListener(this);
-        question = (TextView) findViewById((R.id.textViewQuestion));
+        question = (TextView) findViewById(R.id.textViewQuestion);
+        textWrong = (TextView) findViewById(R.id.textViewWrong);
 
         setUpQuestions();
         showQuestion();
@@ -53,16 +53,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     public void setUpQuestions() {
-        questions.add(new Question(R.drawable.townhall, R.drawable.anglican, R.drawable.larnarch, R.drawable.firstchurch, R.id.textView2, "Click on the Picture of the Anglican Church"));
-        questions.add(new Question(R.drawable.octagon, R.drawable.stkilda, R.drawable.stclair, R.drawable.baldwinst, R.id.textView4, "Click on the Picture of Baldwin Street"));
-        questions.add(new Question(R.drawable.anglican, R.drawable.otagouni, R.drawable.firstchurch, R.drawable.larnarch, R.id.textView3, "Click on the Picture of the First Church"));
-        questions.add(new Question(R.drawable.otagouni, R.drawable.firstchurch, R.drawable.larnarch, R.drawable.townhall, R.id.textView3, "Click on the Picture of the Larnarch Castle"));
-        questions.add(new Question(R.drawable.octagon, R.drawable.baldwinst, R.drawable.trainstation, R.drawable.stclair, R.id.textView1, "Click on the Picture of the Octagon"));
-        questions.add(new Question(R.drawable.townhall, R.drawable.otagouni, R.drawable.larnarch, R.drawable.firstchurch, R.id.textView2, "Click on the Picture of Otago University"));
-        questions.add(new Question(R.drawable.baldwinst, R.drawable.stkilda, R.drawable.octagon, R.drawable.stclair, R.id.textView4, "Click on the Picture of St Clair Beach"));
-        questions.add(new Question(R.drawable.stclair, R.drawable.baldwinst, R.drawable.stkilda, R.drawable.octagon, R.id.textView3, "Click on the Picture of St Kilda Beach"));
-        questions.add(new Question(R.drawable.anglican, R.drawable.townhall, R.drawable.otagouni, R.drawable.trainstation, R.id.textView2, "Click on the Picture of the Town Hall"));
-        questions.add(new Question(R.drawable.trainstation, R.drawable.anglican, R.drawable.otagouni, R.drawable.octagon, R.id.textView1, "Click on the Picture of the Dunedin Train Station"));
+        questions.add(new Question(R.drawable.townhall, R.drawable.anglican, R.drawable.larnarch, R.drawable.firstchurch, R.id.textView2, "Click on the Picture of the Anglican Church", "Town Hall", "Anglican Church", "Larnarch Castle", "First Church"));
+        questions.add(new Question(R.drawable.octagon, R.drawable.stkilda, R.drawable.stclair, R.drawable.baldwinst, R.id.textView4, "Click on the Picture of Baldwin Street", "Octagon", "St Kilda Beach", "St Clair Beach", "Baldwin Street"));
+        questions.add(new Question(R.drawable.anglican, R.drawable.otagouni, R.drawable.firstchurch, R.drawable.larnarch, R.id.textView3, "Click on the Picture of the First Church", "Anglican Church", "Otago University", "First Church", "Larnarch Castle"));
+        questions.add(new Question(R.drawable.otagouni, R.drawable.firstchurch, R.drawable.larnarch, R.drawable.townhall, R.id.textView3, "Click on the Picture of the Larnarch Castle", "Otago University", "First Church", "Larnarch Castle", "Town Hall"));
+        questions.add(new Question(R.drawable.octagon, R.drawable.baldwinst, R.drawable.trainstation, R.drawable.stclair, R.id.textView1, "Click on the Picture of the Octagon", "Octagon", "Baldwin Street", "Train Station", "St Clair Beach"));
+        questions.add(new Question(R.drawable.townhall, R.drawable.otagouni, R.drawable.larnarch, R.drawable.trainstation, R.id.textView2, "Click on the Picture of Otago University", "Town Hall", "Otago University", "Larnarch Castle", "Train Station"));
+        questions.add(new Question(R.drawable.baldwinst, R.drawable.stkilda, R.drawable.octagon, R.drawable.stclair, R.id.textView4, "Click on the Picture of St Clair Beach", "Baldwin Street", "St Kilda Beach", "Octagon", "St Clair Beach"));
+        questions.add(new Question(R.drawable.stclair, R.drawable.baldwinst, R.drawable.stkilda, R.drawable.octagon, R.id.textView3, "Click on the Picture of St Kilda Beach", "St Clair Beach", "Baldwin Street", "St Kilda Beach", "The Octagon"));
+        questions.add(new Question(R.drawable.anglican, R.drawable.townhall, R.drawable.otagouni, R.drawable.trainstation, R.id.textView2, "Click on the Picture of the Town Hall", "Anglican Church", "Town Hall", "Otago University", "Train Station"));
+        questions.add(new Question(R.drawable.trainstation, R.drawable.anglican, R.drawable.otagouni, R.drawable.octagon, R.id.textView1, "Click on the Picture of the Dunedin Train Station", "Train Station", "Anglican Church", "Otago University", "Octagon"));
         Collections.shuffle(questions);
 
     }
@@ -75,6 +75,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         iv2.setBackgroundResource(questions.get(questionNumber).getPicture2());
         iv3.setBackgroundResource(questions.get(questionNumber).getPicture3());
         iv4.setBackgroundResource(questions.get(questionNumber).getPicture4());
+        textWrong.setText("");
         question.setText(questions.get(questionNumber).getQuestion());
         correct = true;
     }
@@ -87,7 +88,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (correct) {
                         score++;
                     }
-                    Toast.makeText(this, "Correct", Toast.LENGTH_LONG).show();
                     questionNumber++;
                     if (questionNumber < 10) {
                         showQuestion();
@@ -95,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } else {
                     iv1.setGravity(Gravity.CENTER);
                     iv1.setBackgroundResource(R.drawable.wrong);
-                    Toast.makeText(this, "Wrong", Toast.LENGTH_LONG).show();
+                    textWrong.setText("Wrong, that was the " + questions.get(questionNumber).getResponse1());
                     correct = false;
                 }
             } else if (v.getId() == R.id.textView2) {
@@ -103,7 +103,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (correct) {
                         score++;
                     }
-                    Toast.makeText(this, "Correct", Toast.LENGTH_LONG).show();
                     questionNumber++;
                     if (questionNumber < 10) {
                         showQuestion();
@@ -111,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } else {
                     iv2.setGravity(Gravity.CENTER);
                     iv2.setBackgroundResource(R.drawable.wrong);
-                    Toast.makeText(this, "Wrong", Toast.LENGTH_LONG).show();
+                    textWrong.setText("Wrong, that was the " + questions.get(questionNumber).getResponse2());
                     correct = false;
                 }
             } else if (v.getId() == R.id.textView3) {
@@ -119,7 +118,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (correct) {
                         score++;
                     }
-                    Toast.makeText(this, "Correct", Toast.LENGTH_LONG).show();
                     questionNumber++;
                     if (questionNumber < 10) {
                         showQuestion();
@@ -127,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } else {
                     iv3.setGravity(Gravity.CENTER);
                     iv3.setBackgroundResource(R.drawable.wrong);
-                    Toast.makeText(this, "Wrong", Toast.LENGTH_LONG).show();
+                    textWrong.setText("Wrong, that was the " + questions.get(questionNumber).getResponse3());
                     correct = false;
                 }
             } else if (v.getId() == R.id.textView4) {
@@ -135,7 +133,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (correct) {
                         score++;
                     }
-                    Toast.makeText(this, "Correct", Toast.LENGTH_LONG).show();
                     questionNumber++;
                     if (questionNumber < 10) {
                         showQuestion();
@@ -143,12 +140,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } else {
                     iv4.setGravity(Gravity.CENTER);
                     iv4.setBackgroundResource(R.drawable.wrong);
-                    Toast.makeText(this, "Wrong", Toast.LENGTH_LONG).show();
+                    textWrong.setText("Wrong, that was the " + questions.get(questionNumber).getResponse4());
                     correct = false;
                 }
             }
         } else {
-            ivA.setBackgroundResource(0);
             //go to a new activity screen
             Intent changeActivity = new Intent(MainActivity.this, Results.class);
             changeActivity.putExtra("results", score);
--- PlacesinNZ/app/src/main/java/thomis1/bit/example/placesinnewzealand/Question.java
+++ PlacesinNZ/app/src/main/java/thomis1/bit/example/placesinnewzealand/Question.java
@@ -7,14 +7,23 @@ public class Question {
     private int picture4;
     private int answer;
     private String question;
+    private String response1;
+    private String response2;
+    private String response3;
+    private String response4;
 
-    public Question(int picture1, int picture2, int picture3, int picture4, int answer, String question) {
+
+    public Question(int picture1, int picture2, int picture3, int picture4, int answer, String question, String response1, String response2, String response3, String response4) {
         this.picture1 = picture1;
         this.picture2 = picture2;
         this.picture3 = picture3;
         this.picture4 = picture4;
         this.answer = answer;
         this.question = question;
+        this.response1 = response1;
+        this.response2 = response2;
+        this.response3 = response3;
+        this.response4 = response4;
     }
 
     public int getPicture1() {
@@ -40,4 +49,12 @@ public class Question {
     public String getQuestion() {
         return question;
     }
+
+    public String getResponse1() { return response1; }
+
+    public String getResponse2() { return response2; }
+
+    public String getResponse3() { return response3; }
+
+    public String getResponse4() { return response4; }
 }
